c7b2fa753bb8b18bcf305e7a93baf98caaf74b39
Chris Prickartz <info@aocalux.com>
2026-05-25 13:45:36 +0200

    Add functions for squaring numbers and greeting

---
+++ 4 Funktionen/funktionen_1.py
@@ -0,0 +1,18 @@
+def quadrat(x):
+    return x * x
+
+print(quadrat(12))
+print(quadrat(14))
+print(quadrat(16))
+print(quadrat(26))
+print(quadrat(4))
+print(quadrat(6))
+
+
+
+def hallo():
+    print("Hallo Welt!")
+    return None
+
+hallo()
+print(hallo())
